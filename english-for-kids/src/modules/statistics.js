997042465919cf1d95366c639a3441e2647a3341
Dmitry Belov <gen-tver@yandex.ru>
2020-04-19 22:23:28 +0300

    feat: add statistics

--- english-for-kids/src/modules/statistics.js
+++ english-for-kids/src/modules/statistics.js
@@ -9,24 +9,46 @@ export default class Statistics {
   }
 
   init() {
+    this.addBtns();
+    this.createTable();
+    this.outputStatistics();
+
+    this.resetBtn.addEventListener('click', () => {
+      this.reset();
+    });
+
+    this.table.querySelector('thead').addEventListener('click', (e) => {
+      if (e.target.closest('th')) {
+        const th = e.target.closest('th');
+        this.sort(th.dataset.sort, th.dataset.desc);
+        th.dataset.desc = th.dataset.desc !== 'true';
+      }
+    });
+  }
+
+  createTable() {
     const tableWrap = document.createElement('div');
     tableWrap.classList.add('table_wrapper');
-    const table = document.createElement('table');
-    table.classList.add('table');
-    tableWrap.append(table);
+    this.table = document.createElement('table');
+    this.table.classList.add('table');
+    tableWrap.append(this.table);
     this.main.append(tableWrap);
-    table.innerHTML = `
+    this.table.innerHTML = `
       <thead>
         <tr>
-          <th>Word</th>
-          <th>Train</th>
-          <th>Success</th>
-          <th>Errors</th>
+          <th data-sort="title" data-desc="true">Word</th>
+          <th data-sort="train" data-desc="false">Train</th>
+          <th data-sort="success" data-desc="false">Success</th>
+          <th data-sort="error" data-desc="false">Errors</th>
         </tr>
       </thead>
     `;
+    this.tbody = document.createElement('tbody');
+  }
+
+  outputStatistics() {
+    this.tbody.innerHTML = '';
     Object.keys(this.statistics).forEach((key) => {
-      console.log(this.statistics[key]);
       const item = this.statistics[key];
       const row = document.createElement('tr');
       row.innerHTML = `
@@ -35,8 +57,9 @@ export default class Statistics {
         <td>${item.success}</td>
         <td>${item.error}</td>
       `;
-      table.append(row);
+      this.tbody.append(row);
     });
+    this.table.append(this.tbody);
   }
 
   create() {
@@ -57,5 +80,41 @@ export default class Statistics {
 
       localStorage.setItem('statistics', JSON.stringify(this.statistics));
     }
+  }
+
+  addBtns() {
+    const btns = document.createElement('div');
+    btns.classList.add('btns');
+    this.resetBtn = document.createElement('button');
+    this.resetBtn.classList.add('stat_btn');
+    this.resetBtn.innerText = 'Reset';
+    this.gameDifficult = document.createElement('button');
+    this.gameDifficult.classList.add('stat_btn');
+    this.gameDifficult.innerText = 'Repeat difficult words';
+    btns.append(this.gameDifficult);
+    btns.append(this.resetBtn);
+    this.main.append(btns);
+  }
+
+  reset() {
+    this.statistics = false;
+    this.create();
+    this.outputStatistics();
+  }
+
+  sort(sorting, desc) {
+    const sortStatistics = Object.keys(this.statistics)
+      .sort((a, b) => {
+        if (desc !== 'true') {
+          return this.statistics[a][sorting] > this.statistics[b][sorting] ? -1 : 1;
+        }
+        return this.statistics[a][sorting] > this.statistics[b][sorting] ? 1 : -1;
+      });
+    const templateStatistics = {};
+    sortStatistics.forEach((item) => {
+      templateStatistics[item] = this.statistics[item];
+    });
+    this.statistics = templateStatistics;
+    this.outputStatistics();
   }
 }
