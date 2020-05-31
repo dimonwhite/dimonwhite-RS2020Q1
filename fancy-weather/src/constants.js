9176e76ae10b5bff9b6de35dfa90fb684e90b024
dimonwhite <gen-tver@yandex.ru>
2020-05-31 22:39:18 +0300

    feat: add format degrees

--- fancy-weather/src/constants.js
+++ fancy-weather/src/constants.js
@@ -3,10 +3,10 @@ const ipinfoKEY = 'aca9dba8741cd7';
 const weatherKEY = 'f97e606ec8f57fbe940782319efd94a6';
 const unsplashKEY = 'SX8BeyCkNJ-RQE81GVX-RH1zIQH4r7f1MhrP59LAO1U';
 
-const ipinfo = `https://ipinfo.io/json?token=${ipinfoKEY}`;
-const opencagedata = `https://api.opencagedata.com/geocode/v1/json?key=${opencagedataKEY}&pretty=1`;
-const weatherAPI = `http://api.openweathermap.org/data/2.5/onecall?&APPID=${weatherKEY}&units=metric`;
-const unsplashAPI = `https://api.unsplash.com/photos/random?orientation=landscape&per_page=1&client_id=${unsplashKEY}&query=`;
+const ipinfo = `//ipinfo.io/json?token=${ipinfoKEY}`;
+const opencagedata = `//api.opencagedata.com/geocode/v1/json?key=${opencagedataKEY}&pretty=1`;
+const weatherAPI = `//api.openweathermap.org/data/2.5/onecall?&APPID=${weatherKEY}&units=metric`;
+const unsplashAPI = `//api.unsplash.com/photos/random?orientation=landscape&per_page=1&client_id=${unsplashKEY}&query=`;
 
 export {
   ipinfo, opencagedata, weatherAPI, unsplashAPI,
